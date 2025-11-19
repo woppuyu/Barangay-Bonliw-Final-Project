174c63dd412fa04a6402776ebdd586920e3bbc4e
woppuyu <pjdimayuga165@gmail.com>
2025-11-19 09:21:30 +0800

    Port update for email

--- services/email.js
+++ services/email.js
@@ -3,8 +3,10 @@ const nodemailer = require('nodemailer');
 // Create email transporter
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: process.env.EMAIL_PORT || 587,
-  secure: false, // true for 465, false for other ports
+  //port: process.env.EMAIL_PORT || 587,
+  port: process.env.EMAIL_PORT || 465,
+  secure: true,
+  //secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
